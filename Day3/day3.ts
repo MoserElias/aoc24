101065fea77b5dae9cd24cf98a80a131117bd3e3
Elias Moser <elias.moser@aol.com>
2024-12-02 09:35:27 +0100

    add Day3 execution class and main function for processing input data

---
+++ Day3/day3.ts
@@ -0,0 +1,20 @@
+const fs3 = require('fs');
+
+class Day3Execution {
+    private readonly inputFilePath: string;
+    private readonly data: string;
+    private readonly lines: string[]; 
+
+    constructor() {
+        this.inputFilePath = "real-input.txt";
+        this.data = fs3.readFileSync(this.inputFilePath, 'utf8');
+        this.lines = this.data.trim().split('\n'); 
+    }
+
+} 
+
+function main3() {
+    const execution = new Day3Execution();
+}
+
+main3();
