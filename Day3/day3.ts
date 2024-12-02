726f6b45cf5e717094ed4822adab7ea75c95a763
Elias Moser <e12116436@student.tuwien.ac.at>
2024-12-02 20:47:48 +0100

    some adaptions for day3.ts

--- Day3/day3.ts
+++ Day3/day3.ts
@@ -1,20 +1,19 @@
-const fs3 = require('fs');
+const fs3 = require("fs");
 
 class Day3Execution {
-    private readonly inputFilePath: string;
-    private readonly data: string;
-    private readonly lines: string[]; 
+  private readonly inputFilePath: string;
+  private readonly data: string;
+  private readonly lines: string[];
 
-    constructor() {
-        this.inputFilePath = "real-input.txt";
-        this.data = fs3.readFileSync(this.inputFilePath, 'utf8');
-        this.lines = this.data.trim().split('\n'); 
-    }
-
-} 
+  constructor() {
+    this.inputFilePath = "real-input.txt";
+    this.data = fs3.readFileSync(this.inputFilePath, "utf8");
+    this.lines = this.data.trim().split("\n");
+  }
+}
 
 function main3() {
-    const execution = new Day3Execution();
+  const execution = new Day3Execution();
 }
 
 main3();
